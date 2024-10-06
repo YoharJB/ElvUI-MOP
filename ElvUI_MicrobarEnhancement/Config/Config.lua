76cde617ebee680ed922542b9244c23b6a26a182
Jeff. <jeffreysfu@gmail.com>
2024-10-06 14:51:54 -0400

    ElvUI MOP

---
+++ ElvUI_MicrobarEnhancement/Config/Config.lua
@@ -0,0 +1,65 @@
+﻿local E, _, V, P, G = unpack(ElvUI)
+local L = E.Libs.ACL:GetLocale("ElvUI", E.global.general.locale)
+local AB = E:GetModule("ActionBars")
+
+function AB:GetOptions()
+	if not E.Options.args.elvuiPlugins then
+		E.Options.args.elvuiPlugins = {
+			order = 50,
+			type = "group",
+			name = "|cff00fcceE|r|cffe5e3e3lvUI |r|cff00fcceP|r|cffe5e3e3lugins|r",
+			args = {}
+		}
+	end
+
+	E.Options.args.elvuiPlugins.args.microbarEnhanced = {
+		type = "group",
+		name = "|cff00fcceM|r|cffe5e3e3icrobar |r|cff00fcceE|r|cffe5e3e3nhancement|r",
+		get = function(info) return E.db.actionbar.microbar[info[#info]] end,
+		set = function(info, value) E.db.actionbar.microbar[info[#info]] = value AB:UpdateMicroPositionDimensions() end,
+		args = {
+			symbolic = {
+				order = 1,
+				type = "toggle",
+				name = L["ENABLE"],
+				desc = L["Replace icons with letters"],
+				disabled = function() return not AB.db.microbar.enabled end
+			},
+			spacer = {
+				order = 2,
+				type = "description",
+				name = ""
+			},
+			transparentButtons = {
+				order = 3,
+				type = "toggle",
+				name = L["Transparent Buttons"],
+				disabled = function() return not AB.db.microbar.enabled or not AB.db.microbar.symbolic end
+			},
+			classColor = {
+				order = 4,
+				type = "toggle",
+				name = L["Use Class Color"],
+				get = function(info) return AB.db.microbar.classColor end,
+				set = function(info, value) AB.db.microbar.classColor = value AB:SetSymbloColor() end,
+				disabled = function() return not AB.db.microbar.enabled or not AB.db.microbar.symbolic end,
+			},
+			color = {
+				order = 5,
+				type = "color",
+				name = L["COLOR"],
+				get = function(info)
+					local t = AB.db.microbar.colorS
+					local d = P.actionbar.microbar.colorS
+					return t.r, t.g, t.b, t.a, d.r, d.g, d.b
+				end,
+				set = function(info, r, g, b)
+					local t = AB.db.microbar.colorS
+					t.r, t.g, t.b = r, g, b
+					AB:SetSymbloColor()
+				end,
+				disabled = function() return not AB.db.microbar.enabled or AB.db.microbar.classColor or not AB.db.microbar.symbolic end
+			}
+		}
+	}
+end
---
+++ ElvUI_MicrobarEnhancement/ElvUI_MicrobarEnhancement.lua
@@ -0,0 +1,79 @@
+﻿local E, L, V, P, G = unpack(ElvUI)
+local AB = E:GetModule("ActionBars")
+local EP = E.Libs.EP
+
+local addonName = ...
+
+local _G = _G
+local pairs = pairs
+
+local PRIEST_COLOR = RAID_CLASS_COLORS.PRIEST
+
+local MICRO_BUTTONS = {
+	CharacterMicroButton = L["CHARACTER_SYMBOL"],
+	SpellbookMicroButton = L["SPELLBOOK_SYMBOL"],
+	TalentMicroButton = L["TALENTS_SYMBOL"],
+	AchievementMicroButton = L["ACHIEVEMENT_SYMBOL"],
+	QuestLogMicroButton = L["QUEST_SYMBOL"],
+	GuildMicroButton = L["GUILD_SYMBOL"],
+	PVPMicroButton = L["PVP_SYMBOL"],
+	LFDMicroButton = L["LFD_SYMBOL"],
+	EJMicroButton = L["JOURNAL_SYMBOL"],
+	CompanionsMicroButton = L["PETS_SYMBOL"],
+	StoreMicroButton = L["STORE_SYMBOL"],
+	HelpMicroButton = L["HELP_SYMBOL"],
+	MainMenuMicroButton = L["MENU_SYMBOL"]
+}
+
+function AB:SetSymbloColor()
+	local colorClass = E:ClassColor(E.myclass) or PRIEST_COLOR
+	local color = AB.db.microbar.classColor and colorClass or AB.db.microbar.colorS
+
+	for button in pairs(MICRO_BUTTONS) do
+		_G[button].text:SetTextColor(color.r, color.g, color.b)
+	end
+end
+
+local oldHandleMicroButton = AB.HandleMicroButton
+function AB:HandleMicroButton(button)
+	oldHandleMicroButton(self, button)
+
+	local text = MICRO_BUTTONS[button:GetName()]
+	button.text = button:CreateFontString(nil, "OVERLAY")
+	button.text:FontTemplate()
+	button.text:Point("CENTER", button, "CENTER", 1, -1)
+	button.text:SetJustifyH("CENTER")
+	button.text:SetText(text)
+end
+
+local oldUpdateMicroPositionDimensions = AB.UpdateMicroPositionDimensions
+
+function AB:UpdateMicroPositionDimensions()
+	oldUpdateMicroPositionDimensions(self)
+
+	local symbol = AB.db.microbar.symbolic
+
+	for button in pairs(MICRO_BUTTONS) do
+		local b = _G[button]
+
+		b.backdrop:SetTemplate(symbol and AB.db.microbar.transparentButtons and "Transparent" or "Default", true)
+
+		b:DisableDrawLayer(symbol and "ARTWORK" or "OVERLAY")
+		b:EnableDrawLayer(symbol and "OVERLAY" or "ARTWORK")
+	end
+
+	GuildMicroButtonTabard.emblem:SetShown(not symbol)
+	GuildMicroButtonTabard.background:SetShown(not symbol)
+
+	AB:SetSymbloColor()
+end
+
+function AB:EnhancementInit()
+	EP:RegisterPlugin(addonName, AB.GetOptions)
+
+	MicroButtonPortrait:SetDrawLayer("ARTWORK", 1)
+	PVPMicroButtonTexture:SetDrawLayer("ARTWORK", 1)
+	MainMenuBarDownload:SetDrawLayer("ARTWORK", 1)
+end
+
+hooksecurefunc(AB, "SetupMicroBar", AB.EnhancementInit)
